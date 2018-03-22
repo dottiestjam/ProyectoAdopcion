a81ff037d7dd373db89b61ec0b0ecec2fa1b4d0f
dottiestjam <dottiestjam1@gmail.com>
2018-03-21 21:34:42 -0700

    Cambiar botones para cerrar modales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,8 +24,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
+      { title: 'Inicio', component: HomePage },
       { title: 'Adopcion', component: AdopcionPage },
       { title: 'Rescate', component: RescatePage}
     ];
